7d81c9034d71c7089969cdcfc8f91a85266c244a
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-05-29 13:27:10 +0300

    Creating an Activity form component

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,19 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetail from "./details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[];
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
     selectedActivity: Activity | undefined;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    editMode: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity, selectedActivity }: Props) {
+export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity, selectedActivity, openForm, closeForm, editMode }: Props) {
     return (
         <Grid container spacing={3}>
             <Grid size={7}>
@@ -19,10 +23,15 @@ export default function ActivityDashboard({ activities, selectActivity, cancelSe
                 />
             </Grid>
             <Grid size={5}>
-                {selectedActivity && <ActivityDetail
-                    activity={selectedActivity}
-                    cancelSelectActivity={cancelSelectActivity}
-                />}
+                {selectedActivity && !editMode &&
+                    <ActivityDetail
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    />}
+                {editMode &&
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />
+                }
             </Grid>
         </Grid>
     )
