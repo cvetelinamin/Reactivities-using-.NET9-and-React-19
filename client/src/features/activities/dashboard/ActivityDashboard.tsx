7e594d565a99f7d6a3da5eb0ffc97f3f9b715470
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-05-29 13:59:25 +0300

    Deleting an activity

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,15 +11,19 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean;
+    submitForm: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity, selectedActivity, openForm, closeForm, editMode }: Props) {
+export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity, selectedActivity, openForm,
+    closeForm, editMode, submitForm, deleteActivity }: Props) {
     return (
         <Grid container spacing={3}>
             <Grid size={7}>
                 <ActivityList
                     activities={activities}
                     selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
                 />
             </Grid>
             <Grid size={5}>
@@ -30,7 +34,7 @@ export default function ActivityDashboard({ activities, selectActivity, cancelSe
                         openForm={openForm}
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} submitForm={submitForm} />
                 }
             </Grid>
         </Grid>
