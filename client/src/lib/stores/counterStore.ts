93820eb94b3bc9a76b0050744035918a574992b5
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-06-01 22:49:35 +0300

    Setting up MobX and using for some examples

---
+++ client/src/lib/stores/counterStore.ts
@@ -0,0 +1,23 @@
+import { action, makeObservable, observable } from 'mobx';
+
+export default class CounterStore {
+    title = 'Counter store';
+    count = 25;
+
+    constructor() {
+        makeObservable(this, {
+            title: observable,
+            count: observable,
+            increment: action,
+            decrement: action
+        })
+    }
+
+    increment = (amount = 1) => {
+        this.count += amount;
+    }
+
+    decrement = (amount = 1) => {
+        this.count -= amount;
+    }
+}
