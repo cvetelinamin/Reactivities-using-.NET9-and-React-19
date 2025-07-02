30b7a198d05029846f9ac0a0f9502c8ba94016aa
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-07-02 22:46:57 +0300

    Creating a user menu with material ui

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -5,6 +5,7 @@ import MenuItemLink from "../shared/components/MenuItemLink";
 import { useStore } from "../../lib/hooks/useStore";
 import { Observer } from "mobx-react-lite";
 import { useAccount } from "../../lib/hooks/useAccount";
+import UserMenu from "./UserMenu";
 
 export default function NavBar() {
   const { uiStore } = useStore();
@@ -28,9 +29,6 @@ export default function NavBar() {
               <MenuItemLink to='/activities'>
                 Activities
               </MenuItemLink>
-              <MenuItemLink to='/createActivity'>
-                Create Activity
-              </MenuItemLink>
               <MenuItemLink to='/counter'>
                 Counter
               </MenuItemLink>
@@ -40,7 +38,7 @@ export default function NavBar() {
             </Box>
             <Box display='flex' alignItems='center'>
               {currentUser ? (
-                <Typography>Welcome {currentUser.displayName}</Typography>
+                <UserMenu />
               ) : (
                 <>
                   <MenuItemLink to="/login">Login</MenuItemLink>
