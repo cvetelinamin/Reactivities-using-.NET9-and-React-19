7b9927a0402368770a5dae36a7442a466f5202a1
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-07-03 22:24:14 +0300

    Creating routes to navigate to login

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -2,18 +2,20 @@ import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
 import type { FieldValues } from "react-hook-form";
+import { useAccount } from "./useAccount";
 
 export const useActivities = (id?: string) => {
     const queryClient = new QueryClient();
+    const { currentUser } = useAccount();
     const location = useLocation();
 
-    const { data: activities, isPending } = useQuery({
+    const { data: activities, isLoading } = useQuery({
         queryKey: ['activities'],
         queryFn: async () => {
             const response = await agent.get<Activity[]>('/activities');
             return response.data;
         },
-        enabled: !id && location.pathname === '/activities'
+        enabled: !id && location.pathname === '/activities' && !!currentUser
     });
 
     const { data: activity, isLoading: isLoadingActivity } = useQuery({
@@ -22,7 +24,7 @@ export const useActivities = (id?: string) => {
             const response = await agent.get<Activity>(`/activities/${id}`);
             return response.data;
         },
-        enabled: !!id
+        enabled: !!id && !!currentUser
     })
 
     const updateActivity = useMutation({
@@ -62,7 +64,7 @@ export const useActivities = (id?: string) => {
 
     return {
         activities,
-        isPending,
+        isLoading,
         updateActivity,
         createActivity,
         deleteActivity,
