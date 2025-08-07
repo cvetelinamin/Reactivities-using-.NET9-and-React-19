a84b6d7c483a549a5b4a12ccf88a8e77ec1ddb88
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2025-08-07 21:51:14 +0300

    Added EditProfile function

--- client/src/lib/hooks/useProfile.ts
+++ client/src/lib/hooks/useProfile.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import agent from "../api/agent";
 import { useMemo } from "react";
+import type { EditProfileSchema } from "../schemas/editProfileSchema";
 
 export const useProfile = (id?: string) => {
     const queryClient = useQueryClient();
@@ -85,7 +86,30 @@ export const useProfile = (id?: string) => {
                 return photos?.filter(x => x.id !== photoId)
             })
         }
-    })
+    });
+
+    const updateProfile = useMutation({
+        mutationFn: async (profile: EditProfileSchema) => {
+            await agent.put(`/profiles`, profile);
+        },
+        onSuccess: (_, profile) => {
+            queryClient.setQueryData(['profile', id], (data: Profile) => {
+                if (!data) return data;
+                return {
+                    ...data,
+                    displayName: profile.displayName,
+                    bio: profile.bio
+                }
+            });
+            queryClient.setQueryData(['user'], (userData: User) => {
+                if (!userData) return userData;
+                return {
+                    ...userData,
+                    displayName: profile.displayName
+                }
+            });
+        }
+    });
 
 
     const isCurrentUser = useMemo(() => {
@@ -100,7 +124,8 @@ export const useProfile = (id?: string) => {
         isCurrentUser,
         uploadPhoto,
         setMainPhoto,
-        deletePhoto
+        deletePhoto,
+        updateProfile
     }
 }
 
