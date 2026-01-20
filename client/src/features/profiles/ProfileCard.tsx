cf19572bbfd7a28d2d7c3c96590a4e1662b25e8f
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2026-01-20 22:15:03 +0200

    Configuring the UI for followings

--- client/src/features/profiles/ProfileCard.tsx
+++ client/src/features/profiles/ProfileCard.tsx
@@ -7,7 +7,6 @@ type Props = {
 }
 
 export default function ProfileCard({ profile }: Props) {
-    const following = false;
 
     return (
         <Link to={`/profiles/${profile.id}`} style={{ textDecoration: 'none' }}>
@@ -26,13 +25,13 @@ export default function ProfileCard({ profile }: Props) {
                             </Typography>
                         )}
 
-                        {following && <Chip size="small" label='Following' color="secondary" variant="outlined" />}
+                        {profile.following && <Chip size="small" label='Following' color="secondary" variant="outlined" />}
                     </Box>
                 </CardContent>
                 <Divider sx={{ mb: 2 }} />
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'start' }}>
                     <Person />
-                    <Typography sx={{ ml: 2 }}>20 Followers</Typography>
+                    <Typography sx={{ ml: 2 }}>{profile.followersCount} Followers</Typography>
                 </Box>
             </Card>
         </Link>
